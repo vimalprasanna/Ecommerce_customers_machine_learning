a6c281f671579273411ba546c4b8f8391c329151
vimalprasanna <44139456+vimalprasanna@users.noreply.github.com>
2018-10-18 12:42:03 +0530

    Add files via upload

---
+++ linear_exercise.py
@@ -0,0 +1,41 @@
+import pandas as pd;
+import numpy as np;
+import matplotlib.pyplot as plt;
+import seaborn as sns;
+from sklearn.model_selection import train_test_split;
+from sklearn.linear_model import LinearRegression;
+from sklearn import metrics;
+df=pd.read_csv('Ecommerce Customers');
+print(df.info());
+#sns.jointplot(x='Time on Website',y='Yearly Amount Spent',data=df);
+#plt.show();
+#sns.jointplot(x='Time on App',y='Yearly Amount Spent',data=df);
+#plt.show();
+#sns.jointplot(x='Time on App',y='Length of Membership',data=df,kind='hex');
+#plt.show();
+#sns.pairplot(data=df);
+#plt.show();
+#sns.lmplot(x='Length of Membership',y='Yearly Amount Spent',data=df);
+#plt.show();
+print(df.columns);
+X=df[['Avg. Session Length', 'Time on App',
+       'Time on Website', 'Length of Membership']];
+y=df['Yearly Amount Spent'];
+X_train,X_test,y_train,y_test=train_test_split(X,y,test_size=0.3,random_state=101);
+lm=LinearRegression();
+lm.fit(X_train,y_train);
+print(lm.coef_);
+predictions=lm.predict(X_test);
+plt.scatter(y_test,predictions);
+plt.xlabel('y_test');
+plt.ylabel('predictions');
+plt.show();
+print(metrics.mean_absolute_error(y_test,predictions));
+print(metrics.mean_squared_error(y_test,predictions));
+print(np.sqrt(metrics.mean_squared_error(y_test,predictions)));
+sns.distplot((y_test-predictions));
+plt.show();
+cdf=pd.DataFrame(lm.coef_,X.columns,columns=['coeff']);
+print(cdf);
+
+
